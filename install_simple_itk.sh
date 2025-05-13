9f61bbac21ef708c0080b227a3786ad679e0efb0
Octanne <contact@octanne.eu>
2025-05-13 21:25:38 +0200

    add new install_simple_itk_file

--- install_simple_itk.sh
+++ install_simple_itk.sh
@@ -1,45 +1,45 @@
 # Create a venv if not already created and activate it
-if [ ! -d ".venv" ]; then
+if [[ ! -d ".venv" ]]; then
     python3 -m venv .venv
 fi
 . .venv/bin/activate
 # Install pip and wheel
 python -m pip install --upgrade pip
 python -m pip install --upgrade wheel
+python -m pip install -r RENDU_Projet/requirement.txt
 
-
-echo "Install LUA if not already  sudo apt install lua5.4"
+echo "Install LUA if not already ex: sudo apt install lua5.4"
 
 # SimpleITK installation script with 
 # Compilation manuelle de ITK
-if [ ! -d "ITK" ]; then
+if [[ ! -d "ITK" ]]; then
 git clone https://github.com/InsightSoftwareConsortium/ITK.git
 cd ITK
 mkdir ITK-build
 cd ITK-build
-cmake ../ -DCMAKE_BUILD_TYPE=Release -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF
+cmake ../ -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_FLAGS="-include cstdint" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF
 make -j8 # Adjust the number of jobs as needed
 cd ../..
 fi
 
 # Compilation manuelle de Elastix
-if [ ! -d "Elastix" ]; then
+if [[ ! -d "Elastix" ]]; then
 git clone https://github.com/SuperElastix/elastix.git Elastix
 cd Elastix
 mkdir build
 cd build
-ITK_DIR="../../ITK/ITK-build" cmake .. -DCMAKE_BUILD_TYPE=Release -DELASTIX_USE_OPENMP=ON
+ITK_DIR="../../ITK/ITK-build" cmake .. -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_FLAGS="-include cstdint" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release -DELASTIX_USE_OPENMP=ON
 make -j8 # Adjust the number of jobs as needed
 cd ../..
 fi
 
 # Compilation manuelle de SimpleITK
-if [ ! -d "SimpleITK" ]; then
+if [[ ! -d "SimpleITK" ]]; then
 git clone https://github.com/SimpleITK/SimpleITK.git
 cd SimpleITK
 mkdir SimpleITK-build
 cd SimpleITK-build
-ITK_DIR="../../ITK/ITK-build" Elastix_DIR="../../Elastix/build" cmake ../ -DSimpleITK_USE_ELASTIX=ON -DCMAKE_BUILD_TYPE=Release
+ITK_DIR="../../ITK/ITK-build" Elastix_DIR="../../Elastix/build" cmake ../ -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_FLAGS="-include cstdint" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DSimpleITK_USE_ELASTIX=ON -DCMAKE_BUILD_TYPE=Release -DWRAP_LUA=OFF
 make -j8 # Adjust the number of jobs as needed
 cmake --build . --config Release
 cd SimpleITK-build/Wrapping/Python
